fe152ea11435eb3dd50e67d83cb738ed03d66bb7
adnan al kharfan <adnanicloud2024@gmail.com>
2024-06-13 23:42:35 +0300

    init laravel server

---
+++ Laravel Server/database/migrations/2021_10_19_152032_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title')->default(null);
+            $table->string('description')->default(null);
+            $table->string('image')->default(null);
+            $table->double("price")->default(null);
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
